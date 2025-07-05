184853b904b3412024a3ea44a857403944416c80
satishthapak <satishthapak@outlook.com>
2025-07-05 16:28:43 +0530

    commited by satish

--- modules/networking/main.tf
+++ modules/networking/main.tf
@@ -1,7 +1,7 @@
 # Setup VPC
 resource "aws_vpc" "vpc" {
   cidr_block = var.cidr_block
-  tags = {
+    tags = {
     Name = var.vpc_name
   }
 }
@@ -13,7 +13,7 @@ resource "aws_subnet" "public_subnet_config" {
   cidr_block        = each.value.cidr
   availability_zone = each.value.az
 
-  tags = {
+    tags = {
     Name = "public-subnet-az-${each.value.az}"
   }
 }
@@ -25,7 +25,51 @@ resource "aws_subnet" "private_subnet_config" {
   cidr_block        = each.value.cidr
   availability_zone = each.value.az
 
-  tags = {
+    tags = {
     Name = "private-subnet-az-${each.value.az}"
   }
+}
+
+# Setup Internet Gateway
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc.id
+    tags = {
+    Name = "jenkins-terra-igw"
+  }
+}
+
+# Public Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id     = aws_vpc.vpc.id
+  route {
+  cidr_block = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.igw.id
+}
+
+    tags = {
+    Name = "jenkis-terra-public_route_table"
+  }
+}
+
+# Public Route Table and public Subnet Association
+resource "aws_route_table_association" "public_assoc" {
+  for_each       = aws_subnet.public_subnet_config
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+# Private Route Table
+resource "aws_route_table" "private_route_table" {
+  vpc_id = aws_vpc.vpc.id
+  
+    tags = {
+    Name = "jenkis-terra-private_route_table"
+  }
+}
+
+# Private Route Table and private Subnet Association
+resource "aws_route_table_association" "private_assoc" {
+  for_each       = aws_subnet.private_subnet_config
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private_route_table.id
 }
--- modules/networking/variable.tf
+++ modules/networking/variable.tf
@@ -1,18 +1,5 @@
-
+variable "cidr_block" {}
+variable "vpc_name" {}
+variable "public_subnet_config" {}
+variable "private_subnet_config" {}
 variable "availability_zone" {}
-
-variable "cidr_block" {
-  description = "CIDR block for the VPC"
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "Name tag for the VPC"
-  type        = string
-}
-variable "public_subnet_config" {
-  
-}
-variable "private_subnet_config" {
-  
-}
