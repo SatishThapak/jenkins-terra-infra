8c809e854ac11742457fa6de6dc7d1e08d89398b
satishthapak <satishthapak@outlook.com>
2025-07-15 15:24:48 +0530

    committed by satish

--- variable.tf
+++ variable.tf
@@ -1,18 +1,9 @@
-variable "cidr_block" {
-  description = "CIDR block for VPC"
-  type        = string
-}
-
-variable "vpc_name" {
-  description = "Name for the VPC"
-  type        = string
-}
-
+variable "cidr_block" {}
+variable "vpc_name" {}
 variable "public_subnet_config" {}
 variable "private_subnet_config" {}
 variable "availability_zone" {}
 
 variable "ami" {}
 variable "instance_type" {}
-# variable "public_key" {}
 variable "instance_name" {}
